1fec95d06f79d776c7a42e4c1e1f810a9cc7c0d1
abo_0423 <abo_0423@163.com>
2019-05-10 13:30:43 +0800

    feign的熔断处理

--- service-feign/src/main/java/com/springeureka/servicefeign/service/SchedualServiceHi.java
+++ service-feign/src/main/java/com/springeureka/servicefeign/service/SchedualServiceHi.java
@@ -1,5 +1,6 @@
 package com.springeureka.servicefeign.service;
 
+import com.springeureka.servicefeign.service.impl.SchudualServiceHiHystric;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,12 +9,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 /**
  * springCloud-feign服务消费者
  * 通过 @FeignClient("服务名xxx") 声明具体的服务提供者xxx
+ *
+ * fallback 对应熔断措施的类 指定的类
+ *
  * @author lnb
  * @date 2019年5月9日16:38:51
  */
-@FeignClient("service-hi")
+@FeignClient(value = "service-hi",fallback = SchudualServiceHiHystric.class)
 public interface SchedualServiceHi {
+
     @RequestMapping(value = "/hi", method = RequestMethod.GET)
     String sayHiFromFeignClientOne(@RequestParam(value = "name") String name);
+
+
 
 }
